e5e77e98f91a8a4a3f2aed4821559ea970da7e41
Tony Bowden <tony@tmtm.com>
2017-01-08 11:08:39 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'csv'
 require 'nokogiri'
@@ -13,7 +14,7 @@ require 'scraped_page_archive/open-uri'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -23,7 +24,7 @@ def dob(text)
 end
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def scrape_list(url)
@@ -38,24 +39,24 @@ def scrape_list(url)
     party, party_id = faction.match(/(.*) \((.*)\)/).captures unless faction.to_s.empty?
 
     data = {
-      id: mp_url.to_s[/(\d+).html$/, 1],
-      name: box.css('h1').text.tidy.sub(/\s*\([^\)]+\)/,''),
+      id:         mp_url.to_s[/(\d+).html$/, 1],
+      name:       box.css('h1').text.tidy.sub(/\s*\([^\)]+\)/, ''),
       other_name: box.xpath('.//td[contains(.,"Naam")]/following-sibling::td').text.tidy,
-      party: party,
-      party_id: party_id,
-      email: box.xpath('.//td[contains(.,"Email")]/following-sibling::td/a/@href').map(&:text).join(';').gsub('mailto:',''),
-      phone: box.xpath('.//td[contains(.,"Telefoon")]/following-sibling::td').text.tidy,
-      fax: box.xpath('.//td[contains(.,"Fax")]/following-sibling::td').text.tidy,
-      facebook: box.xpath('.//td[contains(.,"Facebook")]/following-sibling::td/a/@href').map(&:text).join(';'),
-      twitter: box.xpath('.//td[contains(.,"Twitter")]/following-sibling::td/a/@href').map(&:text).join(';'),
-      linkedin: box.xpath('.//td[contains(.,"Linkedin")]/following-sibling::td/a/@href').text,
+      party:      party,
+      party_id:   party_id,
+      email:      box.xpath('.//td[contains(.,"Email")]/following-sibling::td/a/@href').map(&:text).join(';').gsub('mailto:', ''),
+      phone:      box.xpath('.//td[contains(.,"Telefoon")]/following-sibling::td').text.tidy,
+      fax:        box.xpath('.//td[contains(.,"Fax")]/following-sibling::td').text.tidy,
+      facebook:   box.xpath('.//td[contains(.,"Facebook")]/following-sibling::td/a/@href').map(&:text).join(';'),
+      twitter:    box.xpath('.//td[contains(.,"Twitter")]/following-sibling::td/a/@href').map(&:text).join(';'),
+      linkedin:   box.xpath('.//td[contains(.,"Linkedin")]/following-sibling::td/a/@href').text,
       birth_date: dob(box.xpath('.//td[contains(.,"Geboortedatum")]/following-sibling::td').text),
-      image: box.xpath('.//h1/following-sibling::img[1]/@src').text,
-      term: '7',
-      source: mp_url.to_s,
+      image:      box.xpath('.//h1/following-sibling::img[1]/@src').text,
+      term:       '7',
+      source:     mp_url.to_s,
     }
     data[:image] = URI.join(mp_url, data[:image]).to_s unless data[:image].to_s.empty?
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
