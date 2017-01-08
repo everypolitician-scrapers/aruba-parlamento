7b5346cd240259ba9f8ca80d4067f2e393f5246a
Tony Bowden <tony@tmtm.com>
2017-01-08 11:09:57 +0000

    require Scraped
    We don't actually _use_ it yet, but it provides us with String.tidy and Nokogiri

--- scraper.rb
+++ scraper.rb
@@ -2,21 +2,13 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'csv'
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
+require 'scraped'
 require 'scraperwiki'
 
 # require 'open-uri/cached'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
-
-class String
-  def tidy
-    gsub(/[[:space:]]+/, ' ').strip
-  end
-end
 
 def dob(text)
   return if text.to_s.empty?
